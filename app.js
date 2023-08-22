a147dde6a51b8ef0b4b5244e20e52c1ef8120705
unknown <you@example.com>
2023-08-22 07:27:34 -0700

    creating models and migrations

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-app.get("/",(req,res)=>{
-    res.send("Hello World");
-});
+app.use(bodyParser);
+
 module.exports = app;
 
