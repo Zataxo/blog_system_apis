e1143cc97f82334d913f85f3dcb51dacade1401c
“Zataxo” <hassan.og@outlook.com>
2023-08-24 12:32:28 -0700

    creating user controller + signup function

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const postRoute = require("./routes/post_routes");
+const userRoute = require("./routes/user_routes");
 const commentRoute = require("./routes/comment_routes");
 const app = express();
 app.use(bodyParser.json());
-app.use("/posts",postRoute);
-app.use("/comments",commentRoute);
+app.use("/posts", postRoute);
+app.use("/comments", commentRoute);
+app.use("/users", userRoute);
 
 module.exports = app;
-
---
+++ controllers/user_controller.js
@@ -0,0 +1,62 @@
+const models = require("../models");
+const bcryprtjs = require("bcryptjs");
+const jsw = require("jsonwebtoken");
+const validator = require("fastest-validator");
+
+function signUp(req, res) {
+  const userPass = req.body.password;
+  models.User.findOne({ where: { email: req.body.email } })
+    .then((result) => {
+      if (result) {
+        res.status(409).json({
+          message: "Email already Exists",
+        });
+      } else {
+        bcryprtjs.genSalt(10, function (err, salt) {
+          bcryprtjs.hash(userPass, salt, function (err, hash) {
+            const schema = {
+              name: { type: "string", optional: false },
+              email: { type: "string", optional: false },
+              password: { type: "string", optional: false },
+            };
+            const userModel = {
+              name: req.body.name,
+              email: req.body.email,
+              password: hash,
+            };
+            const v = new validator();
+            const validationResponse = v.validate(userModel, schema);
+            if (validationResponse != true) {
+              res.status(400).json({
+                message: "Validation Failed",
+                error: validationResponse,
+              });
+            } else {
+              models.User.create(userModel)
+                .then((result) => {
+                  res.status(201).json({
+                    message: "User Created Successfully",
+                    result: result,
+                  });
+                })
+                .catch((err) => {
+                  res.status(500).json({
+                    message: "Internal Server Error",
+                    error: err,
+                  });
+                });
+            }
+          });
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "internal Server Error",
+        error: err,
+      });
+    });
+}
+module.exports = {
+  signUp: signUp,
+};
